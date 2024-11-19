8ed7de290752b1b0300a8ce194a87eb4958e57e0
ShockWav3456 <tylovgren.social@gmail.com>
2024-11-18 23:55:53 -0600

    writing tests

--- pathing.py
+++ pathing.py
@@ -2,7 +2,7 @@ import graph_data as graph_data_file
 import global_game_data
 from numpy import random
 import sys
-import heapq as heap
+import heapq
 import math
 
 
@@ -269,92 +269,50 @@ def get_bfs_path():
     return pathStartToEnd
     return [1,2]
 
-class Node:
-
-    def __init__(self, nodeIndex, nodeCoords, adjacent):
-        self.nodeIndex = nodeIndex
-        self.coords = nodeCoords
-        self.adjacent = adjacent
-        self.cost = sys.maxsize
-    
-    
-    def getAdjacent(self):
-        return self.adjacent
-    
-    def getCoords(self):
-        return self.coords
-    
-    def getCost(self):
-        return self.cost
-    
-    def getNode(self):
-        return self
-    
-    def getNodeIndex(self):
-        return self.nodeIndex
-    
-    def setCost(self, newCost):
-        if self.cost > newCost:
-            self.cost = newCost
-            return True
-        return False
-
-
-    
-
 def get_cost(currentNode, neighborNode):
-    neighborX, neighborY = neighborNode.getCoords()
-    currentX, currentY = currentNode.getCoords()
-    return math.sqrt((neighborX - currentX) ** 2 + (neighborY - currentY) ** 2) + currentNode.getCost()
+    neighborX, neighborY = neighborNode[0]
+    currentX, currentY = currentNode[0]
+    return math.sqrt((neighborX - currentX) ** 2 + (neighborY - currentY) ** 2)
 
 
-def update_priority(heapToAlter, old_priority, new_priority, item):
-    for i in range(len(heapToAlter)):
-        if heapToAlter[i][1] == item and heapToAlter[i][0] == old_priority:
-            heapToAlter[i] = (new_priority, item)
-            heap.heapify(heapToAlter)  # Reorder the heap after updating
-            break
-
-def dijkstra_to_target(objIndex, graph, nodes, dijkstraHeap):
+def dijkstra_to_target(objIndex, graph, priority_queue):
     assert(graph is not None), "Graph is None"
     assert(objIndex >= 0 and objIndex < len(graph)), "Target index is not a valid index"
-    assert(dijkstraHeap is not None), "Stack cannot be None"
-
+    objIndexFound = False   
+    cost = [sys.maxsize] * len(graph)
+    cost[0] = 0
     parent = [-1] * len(graph)
-    solved = list()
-    seenNotSolved = list()
-    seenNotSolved.append(dijkstraHeap[0][1])
-    objIndexFound = False
-    currentIndex = -1
-    while(dijkstraHeap and not objIndexFound):
-        # (1): Pop Node
-        currentIndex = heap.heappop(dijkstraHeap)[1]
-        currentNode = nodes[currentIndex]
+    visited = set()
+    
+    
+    
+    while priority_queue:
+        #get smallest distance
+        currentCost, currentIndex = heapq.heappop(priority_queue)
+        #skip visited nodes
+        if currentIndex in visited:
+            continue
         
+        #current node is visited
+        visited.add(currentIndex)
         
-
-        # (2): if curr node not in visited, mark it visited
-        if(currentIndex not in solved):
-            solved.append(currentIndex)
-            seenNotSolved.remove(currentIndex)
-            if(currentIndex == objIndex):
-                objIndexFound = True
-            # (3): Update unvisited neighbors
-            for neighbor in currentNode.getAdjacent():
-                if(neighbor not in solved):
-                    cost = get_cost(currentNode, nodes[neighbor])
-                    if(neighbor not in seenNotSolved):
-                        seenNotSolved.append(neighbor)
-                        parent[neighbor] = currentIndex
-                        heap.heappush(dijkstraHeap, (cost, neighbor))
-                    else:
-                        oldCost = nodes[neighbor].getCost()
-                        if(nodes[neighbor].setCost(cost)):
-                            parent[neighbor] = currentIndex
-                            update_priority(dijkstraHeap, oldCost, cost, neighbor)
-                    
-
-
+        # stop at target node
+        if currentIndex == objIndex:
+            objIndexFound = True
+            break
+        
+        # check neighbors for adjaceny values
+        for neighbor in graph[currentIndex][1]:
+            if neighbor in visited:
+                continue
+            
+            newCost = currentCost + get_cost(graph[currentIndex],graph[neighbor])
+            
+            if newCost < cost[neighbor]:
+                cost[neighbor] = newCost
+                parent[neighbor] = currentIndex
+                heapq.heappush(priority_queue, (newCost, neighbor))
+    
 
     if(objIndexFound):
         path = list()
@@ -363,7 +321,8 @@ def dijkstra_to_target(objIndex, graph, nodes, dijkstraHeap):
             currentIndex = parent[currentIndex]
         path.reverse()
         return path
-    
+    if cost[objIndex] != sys.maxsize:
+        return path 
     return None
 
 
@@ -381,20 +340,10 @@ def get_dijkstra_path():
     #Intializing and declaring variable for Path
     pathStartToEnd = list()
 
-    #Start to Target DFS Path
+    # Min-heap (priority queue) to select the node with the smallest distance
+    priority_queue = [(0, 0)]  # (distance, node)
 
-    #Intializing nodes and heapq for Start to Target Dijkstra
-
-    # Give all nodes cost ∞ and mark them “unseen”
-    nodes = list()
-    for i in range(len(graph)):
-        nodes.append(Node(i, graph[i][0], graph[i][1]))
-
-    heapStartToTarget = []
-    heap.heapify(heapStartToTarget)
-    nodes[0].setCost(0)
-    heap.heappush(heapStartToTarget, (nodes[0].getCost(), 0))
-    pathsToTarget = dijkstra_to_target(targetIndex, graph, nodes, heapStartToTarget)
+    pathsToTarget = dijkstra_to_target(targetIndex, graph, priority_queue)
     #print(pathsToTarget)
     
     
@@ -407,16 +356,8 @@ def get_dijkstra_path():
 
 
     #Target To End
-
-    #Intializing nodes and heapq for Target to End Dijkstra
-    heapTargetToEnd = []
-    heap.heapify(heapTargetToEnd)
-    nodes[targetIndex].setCost(0)
-    heap.heappush(heapTargetToEnd, (nodes[targetIndex].getCost(), targetIndex))
-    #print(heapTargetToEnd)
-
-    #Target to End BFS Path
-    pathTargetToEnd = dijkstra_to_target(endIndex, graph, nodes, heapTargetToEnd)
+    priority_queue = [(0, targetIndex)]
+    pathTargetToEnd = dijkstra_to_target(endIndex, graph, priority_queue)
 
     #Transferring the pathTargetToEnd to pathStartToEnd
     assert(pathTargetToEnd is not None), "Path from target to end is None"
